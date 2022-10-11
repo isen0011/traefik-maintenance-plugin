ca4754f059c1de989039806df25729f115012ccb
Remco Wolvenne <r.wolvenne@programic.com>
2022-10-11 22:31:54 +0200

    Building

--- maintenance.go
+++ maintenance.go
@@ -7,35 +7,82 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 )
-
-var pwd = "/plugins-local/src/github.com/programic/traefik-maintenance-plugin"
 
 type Config struct {
 	InformUrl string `json:"informUrl,omitempty"`
 }
 
+type Host struct {
+	Regex    string
+	AllowIps []string
+}
+
+type Maintenance struct {
+	name     string
+	next     http.Handler
+	config   *Config
+	bodyHtml []byte
+	bodyJson []byte
+}
+
+// Global variables
+var pwd = "/plugins-local/src/github.com/programic/traefik-maintenance-plugin"
+var hosts []Host
+
 func CreateConfig() *Config {
 	return &Config{}
 }
 
+// Read a file from the file system and return the data
 func ReadFile(file string) []byte {
 	data, err := os.ReadFile(pwd + "/" + file)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("ReadFile: %v", err)
 	}
 
 	return data
 }
 
-func getIps(req *http.Request) []string {
+// Inform if there are hosts in maintenance
+func Inform(informUrl string) {
+	t := time.NewTicker(5 * time.Second)
+	for ; true; <-t.C {
+
+		client := http.Client{
+			Timeout: time.Second * 5,
+		}
+
+		req, _ := http.NewRequest(http.MethodGet, informUrl, nil)
+		res, doErr := client.Do(req)
+		if doErr != nil {
+			log.Printf("Inform: %v", doErr) // Don't fatal, just go further
+			continue
+		}
+
+		defer res.Body.Close()
+
+		decoder := json.NewDecoder(res.Body)
+		decodeErr := decoder.Decode(&hosts)
+		if decodeErr != nil {
+			log.Printf("Inform: %v", decodeErr) // Don't fatal, just go further
+			continue
+		}
+
+		log.Printf("Inform response: %v", hosts)
+	}
+}
+
+// Get all the client's ips
+func GetClientIps(req *http.Request) []string {
 	var ips []string
 
 	if req.RemoteAddr != "" {
-		ip, _, err := net.SplitHostPort(req.RemoteAddr)
-		if err != nil {
+		ip, _, splitErr := net.SplitHostPort(req.RemoteAddr)
+		if splitErr != nil {
 			ip = req.RemoteAddr
 		}
 		ips = append(ips, ip)
@@ -51,63 +98,32 @@ func getIps(req *http.Request) []string {
 	return ips
 }
 
-func matchIps(req *http.Request) bool {
+// Check one of the ips has access
+func CheckIpAllowed(req *http.Request, host Host) bool {
+	for _, ip := range GetClientIps(req) {
+		for _, allowIp := range host.AllowIps {
+			if ip == allowIp {
+				return true
+			}
+		}
+	}
 
 	return false
 }
 
-func matchHost(req *http.Request) bool {
-
-	// https://github.com/tomMoulard/fail2ban/blob/v0.6.6/fail2ban.go#L250
-
-	return true
-}
-
-var hosts Hosts
-
-type Host struct {
-	Regex    string
-	AllowIps []string
-}
-
-type Hosts struct {
-	Hosts []Host
-}
-
-func inform(informUrl string) {
-	t := time.NewTicker(5 * time.Second)
-	for ; true; <-t.C {
-
-		client := http.Client{
-			Timeout: time.Second * 5,
-		}
-
-		req, _ := http.NewRequest(http.MethodGet, informUrl, nil)
-		res, doErr := client.Do(req)
-		if doErr != nil {
-			log.Fatal(doErr)
-		}
-
-		defer res.Body.Close()
-
-		decoder := json.NewDecoder(res.Body)
-		decodeErr := decoder.Decode(&hosts)
-		if decodeErr != nil {
-			log.Fatal(decodeErr)
+// Check if the host is under maintenance
+func CheckIfMaintenance(req *http.Request) bool {
+	for _, host := range hosts {
+		if matched, _ := regexp.Match(host.Regex, []byte(req.Host)); matched {
+			return !CheckIpAllowed(req, host)
 		}
 	}
-}
 
-type Maintenance struct {
-	name     string
-	next     http.Handler
-	config   *Config
-	bodyHtml []byte
-	bodyJson []byte
+	return false
 }
 
 func New(_ context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	go inform(config.InformUrl)
+	go Inform(config.InformUrl)
 
 	return &Maintenance{
 		name:     name,
@@ -119,18 +135,7 @@ func New(_ context.Context, next http.Handler, config *Config, name string) (htt
 }
 
 func (a *Maintenance) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-
-	block := false
-
-	log.Println(hosts.Hosts)
-
-	if matchHost(req) {
-		if !matchIps(req) {
-			block = true
-		}
-	}
-
-	if block {
+	if CheckIfMaintenance(req) {
 
 		var body []byte
 
@@ -148,7 +153,7 @@ func (a *Maintenance) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		rw.WriteHeader(http.StatusServiceUnavailable)
 
 		if _, err := rw.Write(body); err != nil {
-			log.Printf("unable to write rewrited body: %v", err)
+			log.Printf("ServeHTTP: %v", err)
 		}
 
 		if flusher, ok := rw.(http.Flusher); ok {
