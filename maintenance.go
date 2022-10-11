323d9099af7a13c201e729ce542cb66e3e26713b
Remco Wolvenne <r.wolvenne@programic.com>
2022-10-11 10:43:00 +0200

    Building

--- maintenance.go
+++ maintenance.go
@@ -3,8 +3,13 @@ package traefik_maintenance_plugin
 import (
 	"context"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"strings"
 )
+
+var pwd = "/plugins-local/src/github.com/programic/traefik-maintenance-plugin"
 
 type Config struct {
 	LastModified bool `json:"lastModified,omitempty"`
@@ -14,35 +19,92 @@ func CreateConfig() *Config {
 	return &Config{}
 }
 
+func ReadFile(file string) []byte {
+	data, err := os.ReadFile(pwd + "/" + file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}
+
+func getIps(req *http.Request) []string {
+	var ips []string
+
+	if req.RemoteAddr != "" {
+		ip, _, err := net.SplitHostPort(req.RemoteAddr)
+		if err != nil {
+			ip = req.RemoteAddr
+		}
+		ips = append(ips, ip)
+	}
+
+	forwardedFor := req.Header.Get("X-Forwarded-For")
+	if forwardedFor != "" {
+		for _, ip := range strings.Split(forwardedFor, ",") {
+			ips = append(ips, strings.TrimSpace(ip))
+		}
+	}
+
+	return ips
+}
+
+func matchIps(req *http.Request) bool {
+
+	return false
+}
+
+func matchHost(req *http.Request) bool {
+
+	// https://github.com/tomMoulard/fail2ban/blob/v0.6.6/fail2ban.go#L250
+
+	return true
+}
+
 type Maintenance struct {
-	name         string
-	next         http.Handler
-	lastModified bool
+	name     string
+	next     http.Handler
+	bodyHtml []byte
+	bodyJson []byte
 }
 
 func New(_ context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 	return &Maintenance{
-		name:         name,
-		next:         next,
-		lastModified: config.LastModified,
+		name:     name,
+		next:     next,
+		bodyHtml: ReadFile("maintenance.html"),
+		bodyJson: ReadFile("maintenance.json"),
 	}, nil
 }
 
 func (a *Maintenance) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	if true {
-		bodyBytes := []byte("Deze pagina is in onderhoud.")
+
+	block := false
+
+	if matchHost(req) {
+		if !matchIps(req) {
+			block = true
+		}
+	}
+
+	if block {
+
+		var body []byte
 
 		rw.Header().Del("Last-Modified")
 		rw.Header().Del("Content-Length")
 
 		if req.Header.Get("Accept") == "application/json" {
-			rw.Header().Set("Content-Type", "application/json")
-			bodyBytes = []byte("{}")
+			rw.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+			body = a.bodyJson
+		} else {
+			body = a.bodyHtml
 		}
 
 		rw.WriteHeader(http.StatusServiceUnavailable)
 
-		if _, err := rw.Write(bodyBytes); err != nil {
+		if _, err := rw.Write(body); err != nil {
 			log.Printf("unable to write rewrited body: %v", err)
 		}
 
